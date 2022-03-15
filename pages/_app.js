bc428ba969a84c554b93560ae953298927c9721b
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2022-03-15 19:46:40 -0300

    teste

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,10 @@ import Head from 'next/head'
 
 import '../styles/main.css'
 
+import Prism from 'prism-react-renderer/prism'
+(typeof global !== "undefined" ? global : window).Prism = Prism
+require("prismjs/components/prism-javascript")
+
 export default function Nextra({ Component, pageProps }) {
   return (
     <>
